bd278b3fa427ba3ba0cae64aa7cdfd56767879c3
Cognitive Mesh <cognitivemesh@gmail.com>
2024-10-02 01:29:10 +0200

    Update tables.sql
    Add increments

--- model/sqlite/tables.sql
+++ model/sqlite/tables.sql
@@ -1,6 +1,20 @@
---- Role: 
-CREATE TABLE Role (
-    RoleID INT PRIMARY KEY,
-    RoleName VARCHAR(50) NOT NULL,
-    RoleDescription TEXT(50) NULL
+--- Roles:  It stores the roles of the team members
+CREATE TABLE IF NOT EXISTS roles (
+    roleID INT PRIMARY KEY,
+    role_name TEXT NOT NULL,
+    role_description TEXT(50) NULL
+    role_updated_at TEXT    NULL    ,
+    role_created_at TEXT    NULL    ,
+);
+
+CREATE TABLE IF NOT EXISTS increments (
+    incrementID INTEGER PRIMARY KEY AUTOINCREMENT,
+    incrementNumber INTEGER NOT NULL,
+    startDate TEXT NOT NULL,
+    endDate TEXT NOT NULL,
+    duration INTEGER NULL,
+    -- calculated working dayss
+    workdays INTEGER NULL,
+    increment_updated_at TEXT    NULL,
+    increment_created_at TEXT    NULL
 );
